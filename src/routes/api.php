fad3ac33a0992668d5203a2bc9ccc9000cc7ebd6
Prashanth Sreepathi <s.prasantreddy94@gmail.com>
2020-07-26 14:30:36 -0500

    integrated laravel passport without any customizations

--- src/routes/api.php
+++ src/routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('hello', function(){
+    $data = array(
+        'message' => 'Hi, from Laravel.',
+        'description' => "Laravel local development instance running on docker."
+    );
+    return response()->json($data, 200);
+});
